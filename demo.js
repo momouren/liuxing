2634348f0caab720d1d987aed04b1056a8cb3b2e
stella <1149715403@qq.com>
2019-12-11 10:46:19 +0800

    N2 git

--- demo.js
+++ demo.js
@@ -2,8 +2,11 @@ const http = require("http");
 
 let server = http.createServer((req,res)=>{
 
-
+    res.write("hhh")
     res.end("hello,world")
 })
 
+server.on("request",(req,res)=>{
+    console.log("hello,world")
+})
 server.listen(8080);
